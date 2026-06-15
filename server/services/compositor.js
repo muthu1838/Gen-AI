436d7d3891c70f38fbc631a02f71a80f90f9e638
muthu1838 <muthumariappan212@gmail.com>
2026-06-15 10:50:41 +0530

    first commit

---
+++ server/services/compositor.js
@@ -0,0 +1,13 @@
+
+
+import sharp from "sharp";
+import fs from "fs";
+
+export async function compositeImage(imageBuffer) {
+  if (!fs.existsSync("output")) fs.mkdirSync("output");
+
+
+  await sharp(imageBuffer)
+    .png()
+    .toFile("output/final.png");
+}
